6ac17ec309cfd9bbf596736602aec14bcda8f7bb
bigboss09niel <ronielmadrigal.rm@gmail.com>
2024-07-16 14:36:53 +0800

    login_back_end

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-
-
       home: SplashScreen(),
     );
   }
